00fa9ae40b53eeb514cf5d8e3bdc872cd5464c72
MayNiklas <info@niklas-steffen.de>
2022-07-09 10:00:58 +0200

    pre-commit run --all

--- src/valorant/main.py
+++ src/valorant/main.py
@@ -15,7 +15,7 @@ def milliseconds_to_time(milliseconds):
     seconds = milliseconds // 1000
     minutes = seconds // 60
     seconds = seconds % 60
-    return '%d:%02d' % (minutes, seconds)
+    return "%d:%02d" % (minutes, seconds)
 
 
 ###############################################################################
@@ -27,14 +27,14 @@ def get_player_json(Username, Tagline):
     Get the json data of a player.
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v2/mmr/eu/'+Username+'/'+Tagline
+    api_url = "https://api.henrikdev.xyz/valorant/v2/mmr/eu/" + Username + "/" + Tagline
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        return(response.json())
+        return response.json()
     else:
-        return(None)
+        return None
 
 
 def get_player_json_by_puuid(puuid):
@@ -42,11 +42,11 @@ def get_player_json_by_puuid(puuid):
     Get the json data of a player by puuid.
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/eu/'+puuid
+    api_url = "https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr/eu/" + puuid
     response = requests.get(api_url)
     data = response.json()
 
-    return(data)
+    return data
 
 
 def get_elo(data):
@@ -54,7 +54,7 @@ def get_elo(data):
     Get the elo of a player.
     """
 
-    return(data['data']['current_data']['elo'])
+    return data["data"]["current_data"]["elo"]
 
 
 def get_rank(data):
@@ -62,17 +62,17 @@ def get_rank(data):
     Get the rank of a player.
     """
 
-    return (data['data']['current_data']['currenttierpatched'])
+    return data["data"]["current_data"]["currenttierpatched"]
 
 
 def get_rank_tier(data):
     """
     Get the rank tier of a player.
     """
-    d = data['data']['current_data']['currenttier']
+    d = data["data"]["current_data"]["currenttier"]
     if not d:
         d = 0
-    return (d)
+    return d
 
 
 def get_puuid(data):
@@ -80,7 +80,7 @@ def get_puuid(data):
     Get the puuid of a player.
     """
 
-    return (data['data']['puuid'])
+    return data["data"]["puuid"]
 
 
 def get_name(data):
@@ -88,7 +88,7 @@ def get_name(data):
     Get the name of a player.
     """
 
-    return (data['data']['name'])
+    return data["data"]["name"]
 
 
 def get_tag(data):
@@ -96,26 +96,32 @@ def get_tag(data):
     Get the tagline of a player.
     """
 
-    return (data['data']['tag'])
+    return data["data"]["tag"]
 
 
 ###############################################################################
 # code related to getting match history
+
 
 def get_matches_json(Username, Tagline):
     """
     Get the last 5 matches that where played by this user
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v3/matches/eu/' + \
-        Username+'/'+Tagline+'?filter=competitive'
+    api_url = (
+        "https://api.henrikdev.xyz/valorant/v3/matches/eu/"
+        + Username
+        + "/"
+        + Tagline
+        + "?filter=competitive"
+    )
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        return(response.json())
+        return response.json()
     else:
-        return(None)
+        return None
 
 
 def get_matches_json_by_puuid(puuid):
@@ -123,15 +129,18 @@ def get_matches_json_by_puuid(puuid):
     Get the last 5 matches that where played by this user
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/' + \
-        puuid+'?filter=competitive'
+    api_url = (
+        "https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/eu/"
+        + puuid
+        + "?filter=competitive"
+    )
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        return(response.json())
+        return response.json()
     else:
-        return(None)
+        return None
 
 
 def get_matchid(data):
@@ -139,7 +148,7 @@ def get_matchid(data):
     Get the matchid of the last match.
     """
 
-    return (data['data'][0]['metadata']['matchid'])
+    return data["data"][0]["metadata"]["matchid"]
 
 
 def get_match_ids(data):
@@ -148,9 +157,10 @@ def get_match_ids(data):
     """
 
     ids = []
-    for i in data['data']:
-        ids.append(i['metadata']['matchid'])
+    for i in data["data"]:
+        ids.append(i["metadata"]["matchid"])
     return ids
+
 
 ###############################################################################
 
@@ -162,14 +172,14 @@ def get_mmr_json(puuid):
     Get the MMR history of a player.
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr-history/eu/' + puuid
+    api_url = "https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr-history/eu/" + puuid
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        return(response.json())
+        return response.json()
     else:
-        return(None)
+        return None
 
 
 def get_mmr_change(data, date):
@@ -177,9 +187,9 @@ def get_mmr_change(data, date):
     Get the MMR change of the last match.
     """
 
-    for match in (data['data']):
-        if (match['date_raw'] == date):
-            return (match['mmr_change_to_last_game'])
+    for match in data["data"]:
+        if match["date_raw"] == date:
+            return match["mmr_change_to_last_game"]
 
 
 def get_mmr_elo(data, date):
@@ -187,9 +197,10 @@ def get_mmr_elo(data, date):
     Get elo of player after the last match.
     """
 
-    for match in (data['data']):
-        if (match['date_raw'] == date):
-            return (match['elo'])
+    for match in data["data"]:
+        if match["date_raw"] == date:
+            return match["elo"]
+
 
 ###############################################################################
 # code related to getting match stats
@@ -200,14 +211,14 @@ def get_match_json(matchid):
     Get the match stats of a match.
     """
 
-    api_url = 'https://api.henrikdev.xyz/valorant/v2/match/' + matchid
+    api_url = "https://api.henrikdev.xyz/valorant/v2/match/" + matchid
 
     response = requests.get(api_url)
 
     if response.status_code == 200:
-        return(response.json())
+        return response.json()
     else:
-        return(None)
+        return None
 
 
 def get_match_metadata(data):
@@ -215,7 +226,7 @@ def get_match_metadata(data):
     Get the metadata of a match
     """
 
-    return (data['data']['metadata'])
+    return data["data"]["metadata"]
 
 
 def get_game_start(data):
@@ -223,7 +234,7 @@ def get_game_start(data):
     Get the time of the start of the last match.
     """
 
-    return (data['data']['metadata']['game_start'])
+    return data["data"]["metadata"]["game_start"]
 
 
 def get_game_length(data):
@@ -231,7 +242,7 @@ def get_game_length(data):
     Get the length of the last match (in milliseconds).
     """
 
-    return (data['data']['metadata']['game_length'])
+    return data["data"]["metadata"]["game_length"]
 
 
 def get_rounds_played(data):
@@ -239,7 +250,7 @@ def get_rounds_played(data):
     Get the length of the last match (in rounds).
     """
 
-    return (data['data']['metadata']['rounds_played'])
+    return data["data"]["metadata"]["rounds_played"]
 
 
 def get_map(data):
@@ -247,22 +258,25 @@ def get_map(data):
     Get the map of the last match.
     """
 
-    return (data['data']['metadata']['map'])
+    return data["data"]["metadata"]["map"]
+
 
 ###############################################################################
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # libary is meant to be used as a module
     # so this is only used for testing
-    matches = get_matches_json('MayNiklas', 'Niki')
+    matches = get_matches_json("MayNiklas", "Niki")
 
     for match in get_match_ids(matches):
         data = get_match_json(match)
 
-        print(f'''
+        print(
+            f"""
         Match started: {datetime.datetime.utcfromtimestamp(get_game_start(data))}
         Match length: {milliseconds_to_time(get_game_length(data))}
         Rounds played: {get_rounds_played(data)}
         Map: {get_map(data)}
-        ''')
+        """
+        )
