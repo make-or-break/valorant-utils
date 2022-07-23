b9b71202ef4c95e4046e38ed8ae396e229dd4158
MayNiklas <info@niklas-steffen.de>
2022-07-23 15:22:39 +0200

    add even more tests

--- src/tests/test_valorant.py
+++ src/tests/test_valorant.py
@@ -15,6 +15,28 @@ class TestValorant(unittest.TestCase):
     def test_get_player_json_fail(self):
         self.assertEqual(valorant.get_player_json("not", "existent"), None)
 
+    # test if get_elo returns a number
+    def test_get_elo(self):
+        self.assertGreaterEqual(
+            valorant.get_elo(valorant.get_player_json("MayNiklas", "Niki")), 0
+        )
+
+    # test if get_elo returns a None if player not found
+    def test_get_elo_fail(self):
+        self.assertEqual(valorant.get_player_json("not", "existent"), None)
+
+    # test if get_rank returns a string
+    def test_get_rank(self):
+        self.assertIsInstance(
+            valorant.get_rank(valorant.get_player_json("MayNiklas", "Niki")), str
+        )
+
+    # todo: test if get_rank returns a None if player not found (code not implemented yet)
+    # def test_get_rank_fail(self):
+    #     self.assertEqual(
+    #         valorant.get_rank(valorant.get_player_json("not", "existent")), None
+    #     )
+
 
 if __name__ == "__main__":
     unittest.main()
